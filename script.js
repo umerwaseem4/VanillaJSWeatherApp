5bbc35c7e409785820267b7011528c2a3bd2f7a0
Umer Waseem <Roothacks8@gmail.com>
2020-05-01 21:36:05 +0500

    refracting code

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// http://api.openweathermap.org/data/2.5/weather?q=lahore&appid=a0b32725b13cf527888484064a6f986c&units=metric
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button = document.querySelector('.submit');
+
+button.addEventListener('click', function (name) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=f670cd60c54c18b3edfa49d0f30c1a88')
+        .then(response => response.json())
+        .then(data => {
+            // var tempValue = data['main']['temp'];
+            // var nameValue = data['name'];
+            // var descValue = data['weather'][0]['description'];
+
+            // main.innerHTML = nameValue;
+            // desc.innerHTML = "Desc - "+descValue;
+            // temp.innerHTML = "Temp - "+tempValue;
+            // input.value ="";
+            console.log(data);
+
+        })
+
+        .catch(err => alert("Wrong city name!"));
+})
