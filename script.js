0b5dbdeafa6cf88f849d26a638e6d147f0f7560f
Umer Waseem <Roothacks8@gmail.com>
2020-05-02 00:04:32 +0500

    final ul

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const input_text = document.getElementById('input-text');
 const submit = document.getElementById('submit');
-const name = document.querySelector('.name');
-const temp = document.querySelector('.temp');
-const desc = document.querySelector('.desc');
+const city = document.querySelector('.name');
+const temprature = document.querySelector('.temp');
+const addidata = document.querySelector('.desc');
 
 function weatherData() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input_text.value + '&appid=f670cd60c54c18b3edfa49d0f30c1a88&units=metric')
         .then(response => response.json())
         .then(data => {
             const cityname = data.name;
-            const cityTemp = data.main.temp;
+            const cityTemp = data.main.temp + ' deg';
             const description = data.weather[0].main;
 
-            temp.innerHTML = cityTemp;
-            name.innerHTML = cityname;
-            desc.innerHTML = description
+            temprature.innerHTML = cityTemp;
+            city.innerHTML = cityname;
+            addidata.innerHTML = description
         })
 
         .catch(err => alert("Wrong city name!"));
